fede4f8aaa2345d2a0aa76ac66511e8349a83f63
PlagueTR <black.plague@icloud.com>
2023-09-16 22:02:17 +0300

    Uploaded 1.0.0 Files

---
+++ QuietRain/Plugin.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+using EFT.EnvironmentEffect;
+
+namespace QuietRain
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class QuietRainPlugin : BaseUnityPlugin
+    {
+
+        public static GameObject Hook;
+        public static QuietRainClass QuietRainClassComponent;
+
+        public static ConfigEntry<float> RainVolume { get; set; }
+
+        private void Awake()
+        {
+            Debug.LogError("Quiet Rain Awake()");
+            Hook = new GameObject();
+            QuietRainClassComponent = Hook.AddComponent<QuietRainClass>();
+            DontDestroyOnLoad(Hook);
+        }
+
+        private void Start()
+        {
+            RainVolume = Config.Bind("World", "Rain Volume", 1.0f, new ConfigDescription("", null, new ConfigurationManagerAttributes { Order = 100, IsAdvanced = false }));
+
+            new QuietRainPatch().Enable();
+        }
+
+        public class QuietRainPatch : ModulePatch
+        {
+            protected override MethodBase GetTargetMethod()
+            {
+                return typeof(EnvironmentManager).GetMethod("Init", BindingFlags.Instance | BindingFlags.NonPublic);
+            }
+            [PatchPrefix]
+            private static void PatchPreFix(ref EnvironmentManager __instance)
+            {
+                QuietRainClass.environmentManager = __instance;
+                Traverse.Create(__instance).Field("OutdoorRainVolume").SetValue(RainVolume.Value);
+                Traverse.Create(__instance).Field("RainVolume").SetValue(RainVolume.Value * 0.7f);
+            }
+        }
+
+    }
+}
---
+++ QuietRain/QuietRainClass.cs
@@ -0,0 +1,37 @@
+﻿using EFT;
+using EFT.EnvironmentEffect;
+using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace QuietRain
+{
+    public class QuietRainClass : MonoBehaviour
+    {
+
+        public static LocalPlayer localPlayer;
+        public static EnvironmentManager environmentManager = null;
+
+        public void Start()
+        {
+            QuietRainPlugin.RainVolume.SettingChanged += SettingsUpdated;
+        }
+
+        public void UpdateQuietRain()
+        {
+            if (environmentManager != null)
+            {
+                MethodInfo Init = environmentManager.GetType().GetMethod("Init", BindingFlags.Instance | BindingFlags.NonPublic);
+                Init.Invoke(environmentManager, null);
+                MethodInfo method_4 = environmentManager.GetType().GetMethod("method_4", BindingFlags.Instance | BindingFlags.NonPublic);
+                method_4.Invoke(environmentManager, null);
+            }
+        }
+
+        private void SettingsUpdated(object sender, EventArgs e)
+        {
+            UpdateQuietRain();
+        }
+
+    }
+}
